2e61164d6eef919a7b03b28ad7a23670726e0877
psj45633 <psj45633@gmail.com>
2026-01-07 01:08:17 +0900

    feat : build, upgrade UI On/Off

---
+++ Assets/01_Scripts/UI/MenuButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] private GameObject buildMenu;
+    [SerializeField] private GameObject upgradeMenu;
+    [SerializeField] private GameObject inGameMenu;
+
+    void Start()
+    {
+        buildMenu.SetActive(false);
+        upgradeMenu.SetActive(false);
+        inGameMenu.SetActive(true);
+    }
+
+    public void ActiveSelfBuildMenu()
+    {
+        buildMenu.SetActive(!buildMenu.activeSelf);
+        inGameMenu.SetActive(!inGameMenu.activeSelf);
+    }
+
+    public void ActiveSelfUpgradeMenu()
+    {
+        upgradeMenu.SetActive(!upgradeMenu.activeSelf);
+        inGameMenu.SetActive(!inGameMenu.activeSelf);
+    }
+
+}
